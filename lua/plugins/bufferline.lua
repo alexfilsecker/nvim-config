a21c31573775506d49cba7aced7fd62aecd58e16
Alex Falabella <afilsecker@falabella.cl>
2025-12-16 17:22:11 -0300

    feat: new keymaps for bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -24,6 +24,7 @@ return {
 
     local keymap = vim.keymap.set
 
+    -- Cycling
     keymap(
       "n",
       "<leader>bn",
@@ -38,9 +39,57 @@ return {
     )
     keymap(
       "n",
+      "<leader>bb",
+      "<cmd>BufferLinePick<CR>",
+      { desc = "Pick Buffer" }
+    )
+
+    -- Reordering
+    keymap(
+      "n",
+      "<leader>bl",
+      "<cmd>BufferLineMoveNext<CR>",
+      { desc = "Move buffer to next" }
+    )
+    keymap(
+      "n",
+      "<leader>bh",
+      "<cmd>BufferLineMovePrev<CR>",
+      { desc = "Move buffer to prev" }
+    )
+
+    -- Closing
+    keymap(
+      "n",
+      "<leader>bxl",
+      "<cmd>BufferLineCloseRight<CR>",
+      { desc = "Close buffer to the right" }
+    )
+    keymap(
+      "n",
+      "<leader>bxh",
+      "<cmd>BufferLineCloseLeft<CR>",
+      { desc = "Close buffer to the left" }
+    )
+    keymap(
+      "n",
+      "<leader>bxo",
+      "<cmd>BufferLineCloseOthers<CR>",
+      { desc = "Close all other buffers" }
+    )
+    keymap(
+      "n",
       "<leader>bc",
       "<cmd>BufferLinePickClose<CR>",
       { desc = "Close Buffer" }
+    )
+
+    -- Pinning
+    keymap(
+      "n",
+      "<leader>bP",
+      "<cmd>BufferLineTogglePin<CR>",
+      { desc = "Toggle pin on buffer" }
     )
   end,
 }
